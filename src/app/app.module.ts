6e77fe9355ee0176bcb1b0c0e3fcaa7e4c951891
Juan Alfredo <jgutierrez_pe@outlook.com>
2020-12-12 22:07:19 -0500

    Dashboard AngularMaterial 11.0.2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
+//-Paso : 01
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+//Paso 04 Instalar npm install @angular/flex-layout --save
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { MaterialModule } from './material/material.module';
+import { LayoutComponent } from './layout/layout.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { HomeComponent } from './home/home.component';
+import { RoutingModule } from './routing/routing.module';
+import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
+import { HeaderComponent } from './navigation/header/header.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    HomeComponent,
+    HeaderComponent,
+    SidenavListComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule,    
+    BrowserAnimationsModule,
+    MaterialModule,
+    FlexLayoutModule,
+    RoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
